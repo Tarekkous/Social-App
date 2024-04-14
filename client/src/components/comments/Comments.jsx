ace0b0ad4d75adcdf819c09cd5b69cd26fd5e186
Tarekkous <tarek.koussaier@yahoo.fr>
2024-04-14 18:11:59 +0200

    Adding comment

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,13 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./comments.scss";
 import { AuthContext } from "../../context/authContext";
 import { makeRequest } from "../../axios";
-import { useQuery } from 'react-query'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 import moment from "moment";
 
 
 const Comments = ({ postId }) => {
+
   const { currentUser } = useContext(AuthContext);
+  const [desc,setDesc] = useState("")
 
 
   const { isLoading, error, data } = useQuery("comments", () =>
@@ -16,22 +18,40 @@ const Comments = ({ postId }) => {
     })
   )
 
-console.log(data)
+console.log("comments",data)
 
+const queryClient = useQueryClient()
+
+  //!Mutation pour créer un commentaire , et réactualiser la fonction get (refetch data)
+  const mutation = useMutation((newComment) => {
+    return makeRequest.post("/comments", newComment);
+  }, {
+    onSuccess: () => {
+      //refetch data already getted before in posts.jsx
+      queryClient.invalidateQueries("comments")
+    },
+  })
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc, postId });
+    setDesc = ("")
+
+  };
 
 
   return (
-    <div className="comments">
+    <div className="comments" >
       <div className="write">
         <img src={currentUser.profilePic} alt="" />
-        <input type="text" placeholder="write a comment" />
-        <button>Send</button>
+        <input type="text" placeholder="write a comment" onChange={e => setDesc(e.target.value)} value={desc} />
+        <button onClick={handleClick}>Send</button>
       </div>
 
       {isLoading? "loading.." : data.map((comment) => (
 
-        <div className="comment">
-          <img src={comment.profilePicture} alt="" />
+        <div className="comment" key={comment.id}>
+          <img src={comment.profilePic} alt="" />
           <div className="info">
             <span>{comment.name}</span>
             <p>{comment.desc}</p>
